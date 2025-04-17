ebcfb7f680401d4286d0d45ffa85e8e5b3bd905c
하엽 송 <tpshy87@gmail.com>
2025-04-17 18:43:30 +0900

    알고풀이

--- Algo/pro_lv2_hash2.h
+++ Algo/pro_lv2_hash2.h
@@ -1,1 +1,33 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int solution(vector<int> topping) {
+    int answer = 0;
+
+    unordered_map<int, int> rightCnt;
+    for (int i = 0; i < topping.size(); i++) {
+        rightCnt[topping[i]]++;
+    }
+
+    unordered_set<int> leftSet;
+    for (int i = 0; i < topping.size(); i++) {
+        int now = topping[i];
+        leftSet.insert(now);
+
+        rightCnt[now]--;
+        if (rightCnt[now] == 0) {
+            rightCnt.erase(now);
+        }
+
+        if (leftSet.size() == rightCnt.size()) {
+            answer++;
+        }
+    }
+
+    return answer;
+}
