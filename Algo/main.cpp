727ce59a66beb6e8d8b112ea1f7e94a85c0c1f55
하엽 송 <tpshy87@gmail.com>
2025-05-25 11:06:42 +0900

    5/25/3

--- Algo/main.cpp
+++ Algo/main.cpp
@@ -1,41 +1,30 @@
-﻿#include <iostream>
-#include <vector>
-
+﻿#include<iostream>
+#include<stack>
+#include<string.h>
 using namespace std;
+int main()
+{
+	stack<int> s;
+	string str;
 
-int t;
-int n, m;
-long long arr[1005][1005];
-int mod = 1000000009;
-
-long long Back(int n, int m) {
-    if (n == 0) return 1;
-    if (n < 0 || m == 0) return 0;
-
-    if (arr[n][m] != -1) {
-        return arr[n][m];
-    }
-
-    long long num = 0;
-
-    for (int i = 1; i < 4; i++) {
-        num += Back(n - i, m - 1) % mod;
-    }
-
-    arr[n][m] = num % mod;
-
-    return arr[n][m] % mod;
-}
-
-int main() {
-    cin >> t;
-    memset(arr, -1, sizeof(arr));
-
-    while (t--) {
-        cin >> n >> m;
-        cout << Back(n, m) << '\n';
-    }
-
-
-    return 0;
+	int cnt = 0;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == '(' && str[i + 1] == ')')
+		{
+			cnt += s.size();
+			i++;
+		}
+		else if (str[i] == '(')
+		{
+			s.push(i);
+		}
+		else if (str[i] == ')')
+		{
+			cnt++;
+			s.pop();
+		}
+	}
+	cout << cnt;
 }
