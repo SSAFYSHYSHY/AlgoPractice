d5aab0c7553fc909549d1a67d57e7490890dec45
하엽 송 <tpshy87@gmail.com>
2025-06-02 12:35:12 +0900

    6/2/3

--- Algo/Backjoon_Simul_Silver2_2.h
+++ Algo/Backjoon_Simul_Silver2_2.h
@@ -1,1 +1,27 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric> 
+
+using namespace std;
+
+int main() {
+    int N;
+    long long S;
+    cin >> N >> S;
+
+    vector<long long> A(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i];
+        A[i] = abs(A[i] - S);
+    }
+
+    long long D = A[0];
+    for (int i = 1; i < N; ++i) {
+        D = gcd(D, A[i]);
+    }
+
+    cout << D << '\n';
+    return 0;
+}
