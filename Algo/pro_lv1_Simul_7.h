c6e7050633b4b40d3e39334a5ad3c220fed87e39
하엽 송 <tpshy87@gmail.com>
2025-06-03 10:47:02 +0900

    6/3/2

---
+++ Algo/pro_lv1_Simul_7.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    for (int i = n; i >= 1; i--) {
+        if (n % i == 1) {
+            answer = i;
+        }
+    }
+
+    return answer;
+}
---
+++ Algo/pro_lv1_Simul_9.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+
+    answer = stoi(s);
+
+
+    return answer;
+}
