9905d0701c013da5500e888f805b5939e3d73678
하엽 송 <tpshy87@gmail.com>
2025-06-20 15:54:22 +0900

    6/20/1

--- Algo/Backjoon_Greedy_Bronze1_6.h
+++ Algo/Backjoon_Greedy_Bronze1_6.h
@@ -1,27 +1,33 @@
-﻿#include <string>
+#pragma once
+#include <string>
 #include <iostream>
+
 using namespace std;
 
 string s;
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
 
+int main() {
 	int n, m;
 	cin >> n >> m;
 	cin >> s;
+
 	for (int i = 0; i < m; i++) {
 		string x;
 		cin >> x;
-		bool c = false;
+		bool flag = false;
+
 		for (int j = 0, idx = 0; x[j]; j++) {
 			if (x[j] == s[idx]) idx++;
+
 			if (idx == n) {
-				c = true;
+				flag = true;
 				break;
 			}
 		}
-		if (c) cout << "true\n";
-		else cout << "false\n";
+
+		if (flag)
+			cout << "true\n";
+		else
+			cout << "false\n";
 	}
 }
