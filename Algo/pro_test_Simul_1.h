7f6ad0dc33a9b526e9760beabdae6ba99d0ebfe5
하엽 송 <tpshy87@gmail.com>
2025-07-14 15:56:11 +0900

    7/14/3

--- Algo/pro_test_Simul_1.h
+++ Algo/pro_test_Simul_1.h
@@ -8,56 +8,34 @@ using namespace std;
 vector<vector<int>> attack;
 
 int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
-    int answer = 0;
+    int answer = 0, e = 0, hp = health;
 
     int t = bandage[0];
     int x = bandage[1];
     int y = bandage[2];
 
-    int sum = 0;
     for (int i = 0; i < attacks.size(); i++) {
+        int s = attacks[i][0];
+        int damage = attacks[i][1];
 
-        //초기 적의 구조에 대해서.
-        if (i == 0) {
-            int s = 0;
-            int e = attacks[i][0];
-            int hp = attacks[i][1];
-
-            //연산.
-            int num = e - s - 1;
-            int mod = num / t;
-
-            sum = health + (mod * y) + (num * x);
-            if (sum > health) {
-                sum = health;
+        int num = s - e - 1;
+        if (num > 0) {
+            int mod = (num / t) * y;
+            hp += num * x + mod;
+            if (hp > health) {
+                hp = health;
             }
-
-            //감산.
-            sum -= hp;
-        }
-        //나머지 연산에 대해서.
-        else {
-            int s = attacks[i - 1][0];
-            int e = attacks[i][0];
-            int hp = attacks[i][1];
-
-            //연산.
-            int num = e - s - 1;
-            int mod = num / t;
-
-            sum = sum + (mod * y) + (num * x);
-            if (sum > health) {
-                sum = health;
-            }
-
-            sum -= hp;
         }
 
-        if (sum <= 0) {
+        hp -= damage;
+
+        if (hp <= 0) {
             return -1;
         }
+
+        e = s;
     }
 
-    answer = sum;
+    answer = hp;
     return answer;
 }
