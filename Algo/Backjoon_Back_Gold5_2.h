f04dea9bae29612c62f71d4b0b317d49c7aa0889
하엽 송 <tpshy87@gmail.com>
2025-09-28 13:47:21 +0900

    9/28/1

---
+++ Algo/Backjoon_Back_Gold5_2.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+string str;
+int max_num, len;
+int arr[51];
+vector<int>v;
+
+
+void func(int n) {
+    if (n == len) {
+        for (int i = 0; i < max_num; i++) {
+            cout << v[i] << " ";
+        }
+        exit(0);
+    }
+    int num;
+    string s = "";
+
+    for (int i = n; i <= n + 1; i++) {
+
+        s = s + str[i];
+        num = stoi(s);
+        if (num > max_num) continue;
+        if (arr[num])continue;
+        v.push_back(num);
+        arr[num] = 1;
+        func(i + 1);
+        arr[num] = 0;
+        v.pop_back();
+
+    }
+
+}
+
+
+int main() {
+    cin >> str;
+    len = str.length();
+
+    if (len > 9)
+        max_num = 9 + (len - 9) / 2;
+    else
+        max_num = len;
+
+    for (int i = 1; i <= 50; i++)arr[i] = 0;
+
+    func(0);
+}
