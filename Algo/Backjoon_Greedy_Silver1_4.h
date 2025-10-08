b972cfed9195bf08eaf726cbfb3747a6ebb5bee9
하엽 송 <tpshy87@gmail.com>
2025-10-08 19:11:17 +0900

    10/8/3

---
+++ Algo/Backjoon_Greedy_Silver1_4.h
@@ -0,0 +1,42 @@
+#pragma once
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main() {
+	int N;
+	string str;
+
+	cin >> N;
+	cin >> str;
+
+	stack<int> s;
+
+	int result = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (s.empty()) {
+			s.push(str[i]);
+		}
+		else {
+			if (str[i] == '(' && s.top() == ')') {
+				s.pop();
+			}
+			else if (str[i] == ')' && s.top() == '(') {
+				s.pop();
+			}
+			else {
+				s.push(str[i]);
+			}
+		}
+		result = max(result, (int)s.size());
+	}
+
+	if (s.empty()) {
+		cout << result;
+	}
+	else {
+		cout << -1;
+	}
+}
