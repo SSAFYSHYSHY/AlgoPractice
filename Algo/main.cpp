57b1c12f11a537c6178cb703db43e0e73632cd57
하엽 송 <tpshy87@gmail.com>
2025-10-19 15:24:04 +0900

    10/19/2

---
+++ Algo/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int N, Q;
+bool visited[1100000];
+
+void DFS(int q, int& res) {
+    if (visited[q]) {
+        res = q;
+    }
+    if (q == 1) {
+        return;
+    }
+    DFS(q / 2, res);
+}
+
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
+    cin >> N >> Q;
+
+    for (int i = 0; i < Q; i++) {
+        int q; 
+        cin >> q;
+        
+        int res = 0;
+        DFS(q, res);
+        
+        if (res == 0) {
+            visited[q] = true;
+        }
+        cout << res << '\n';
+    }
+
+}
